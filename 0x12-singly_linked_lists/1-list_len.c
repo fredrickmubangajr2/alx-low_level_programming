87eee8e7f64b469e9f3bf10109965a47d136ff76
fredrickmubangajr2 <fredrick.mubanga.jr2@gmail.com>
2023-05-07 18:49:45 -0700

    betty correction

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -5,7 +5,6 @@
  * @h: pointer to the list_t list
  * Return: number of elements in h
  */
-
 size_t list_len(const list_t *h)
 {
 	size_t n = 0;
@@ -17,4 +16,3 @@ size_t list_len(const list_t *h)
 	}
 	return (n);
 }
-
